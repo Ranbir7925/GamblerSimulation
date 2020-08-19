458e92be4bc800a4b4aa024013129dc801a53b90
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-19 15:34:57 +0530

    MODIFIED:Created individual functions for all operations

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -11,17 +11,38 @@ loss=0
 BET=1
 MIN_STAKE_VALUE=$(( $stake * 50/100 ))
 MAX_STAKE_VALUE=$(( $stake + $MIN_STAKE_VALUE ))
+MAX_GAMBLING_DAYS=20
 
-#Playing till stake value reaches to its limit
-while (($stake<$MAX_STAKE_VALUE && $stake>$MIN_STAKE_VALUE ))
-do
-	if (( $((RANDOM%2)) == 1 ))
+#Function to play till the gambler either win or lose 50 percent of the stake
+function gambling()
+{
+	for (( i=1;i<=20;i++ ))
+	do
+		tempStake=$stake
+		while (($stake<$MAX_STAKE_VALUE && $stake>$MIN_STAKE_VALUE ))
+		do
+			if (( $((RANDOM%2)) == 1 ))
+			then
+				((stake++))
+			else
+				((stake--))
+			fi
+		done
+		dailyResult
+	done
+}
+
+#Function to check how much the amount the gambler has won or lost in that day
+function dailyResult()
+{
+	if (( stake == MAX_STAKE_VALUE ))
 	then
-		((win++))
-		((stake++))
-	else
-		((loss++))
-		((stake--))
+		echo "Total amount won at day$i is $stake$"
+	elif (( stake == MIN_STAKE_VALUE ))
+	then
+		echo "Total amount lost at day$i is $(( tempStake -stake ))$"
 	fi
-done
+	stake=100
+}
 
+gambling
