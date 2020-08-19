25d7fb31984ea8811432949a591e09121f0144c1
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-19 13:47:25 +0530

    ADDED: Initial amount is declared

---
+++ GamblingSimulator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+echo "************************************WELCOME TO THE GAMBLER'S SIMULATION**************************************************"
+
+#VARIABLE
+stake=100
+
+#CONSTANT
+bet=1
