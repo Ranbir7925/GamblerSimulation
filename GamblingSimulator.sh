281c16855bf85bba84f24b418d144e31516f18a0
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-19 13:55:59 +0530

    ADDED:Making a bet of 1$ and checked if won or lost

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,6 +4,15 @@ echo "************************************WELCOME TO THE GAMBLER'S SIMULATION***
 
 #VARIABLE
 stake=100
+win=0
+loss=0
 
 #CONSTANT
 bet=1
+
+if (( $((RANDOM%2)) == 1 ))
+then
+	((win++))
+else
+	((loss++))
+fi
