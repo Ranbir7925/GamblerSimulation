82d544e30d40ddef554c1c5a8fc5be3e0d30d161
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-20 14:12:44 +0530

    ADDED:Determined the gambler's luckiest and unlukiest day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -3,21 +3,24 @@
 echo "************************************WELCOME TO THE GAMBLER'S SIMULATION**************************************************"
 
 declare -A day
+declare -A result
 
 #VARIABLE
 amount=0
+tempStake=0
+
 
 #CONSTANT
 STAKE=100
 BET=1
 MIN_STAKE_VALUE=$(( $STAKE * 50 / 100 ))
 MAX_STAKE_VALUE=$(( $STAKE + $MIN_STAKE_VALUE ))
-MAX_GAMBLING_DAYS=20
+MAX_GAMBLING_DAYS=30
 
 #Function to play till the gambler either win or lose 50 percent of the stake
 function gambling()
 {
-	for (( i=1;i<=20;i++ ))
+	for (( i=1;i<=MAX_GAMBLING_DAYS;i++ ))
 	do
 		#Storing the original stake value to compare the gain and losses
 		tempStake=$STAKE
@@ -41,16 +44,20 @@ function dailyResult()
 {
 	if (( tempStake == MAX_STAKE_VALUE ))
 	then
+		echo "Total amount won at day $i is "$(( tempStake - STAKE  ))
 		day[won]="${day[won]} $i"
 		wins=$(( tempStake - STAKE))
 		amount=$(( amount + wins ))
 
-	elif (( tempStake == MIN_STAKE_VALUE ))
-	then
+	else
+		echo "Total amount lost at day $i is "$(( STAKE - tempStake))
 		day[lost]="${day[lost]} $i"
 		loss=$(( STAKE - tempStake ))
 		amount=$(( amount - loss ))
 	fi
+
+	#Storing the days and the min and max amount it holds
+	result[$i]=$amount
 }
 
 
@@ -63,8 +70,16 @@ function totalAmount()
 	else
 		echo "Amount lost in a month is $amount"
 	fi
+
 	#reinstating the updated value to zero for next simulation
 	amount=0
+
+	echo "The Luckiest Day and Amount is resp:"
+	luckyOrunlucky head
+	echo "The unLuckiest Day and Amount is resp:"
+	luckyOrunlucky tail
+
+
 }
 
 #Function to check the days won and lost
@@ -74,6 +89,16 @@ function wonOrLose()
     do
         echo "Days $i -------->${day[$i]}"
     done
+}
+
+
+#Function to determine the luckiest and unluckiest day
+function luckyOrunlucky()
+{
+	for j in ${!result[@]}
+	do
+		echo "$j ${result[$j]}"
+	done | sort -k2 -rn | $1 -1
 }
 
 read -p "Press 1 to Play: " choice
