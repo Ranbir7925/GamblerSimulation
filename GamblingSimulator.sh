6f00d6cbe9daa12b25175876eab6b5a5e65ffe1b
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-20 14:46:28 +0530

    ADDED:Function for checking that gambler will continue or not

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -16,6 +16,7 @@ BET=1
 MIN_STAKE_VALUE=$(( $STAKE * 50 / 100 ))
 MAX_STAKE_VALUE=$(( $STAKE + $MIN_STAKE_VALUE ))
 MAX_GAMBLING_DAYS=30
+MAX_LOSING_AMOUNT=0
 
 #Function to play till the gambler either win or lose 50 percent of the stake
 function gambling()
@@ -35,7 +36,7 @@ function gambling()
 		done
 		dailyResult
 	done
-	wonOrLose
+	wonOrLost
 	totalAmount
 }
 
@@ -64,15 +65,12 @@ function dailyResult()
 #Function to check total amount remaining at the end of the month
 function totalAmount() 
 {
-	if (( amount>0 ))
+	if (( amount > MAX_LOSING_AMOUNT ))
 	then
 		echo "Amount won in a month is $amount"
 	else
 		echo "Amount lost in a month is $amount"
 	fi
-
-	#reinstating the updated value to zero for next simulation
-	amount=0
 
 	echo "The Luckiest Day and Amount is resp:"
 	luckyOrunlucky head
@@ -83,7 +81,7 @@ function totalAmount()
 }
 
 #Function to check the days won and lost
-function wonOrLose()
+function wonOrLost()
 {
     for i in ${!day[@]}
     do
@@ -95,18 +93,33 @@ function wonOrLose()
 #Function to determine the luckiest and unluckiest day
 function luckyOrunlucky()
 {
-	for j in ${!result[@]}
+	for counter in ${!result[@]}
 	do
-		echo "$j ${result[$j]}"
+		echo "$counter ${result[$counter]}"
 	done | sort -k2 -rn | $1 -1
 }
 
+#reinstating the updated values to zero for next simulation
+function reinstate()
+{
+	amount=0
+	unset day[won]
+	unset day[lost]
+}
+
+
 read -p "Press 1 to Play: " choice
+
 while ((choice == 1))
 do
 	gambling
-	read -p "Press 1 to Play Agian: " choice
-	unset day[won]
-	unset day[lost]
+	if (( amount <= MAX_LOSING_AMOUNT ))
+	then
+		break
+	fi
+	read -p "You've won $amount$ last time...Would you like to play again, If yes then press 1: " choice
+	reinstate
+
 done
-echo "EXITING SIMULATION GOODBYE...!!!"
+
+echo "You've lost all the money...GOODBYE!!"
